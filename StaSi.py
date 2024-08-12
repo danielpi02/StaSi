e1f39b519d57aae4ac675fb3d785e84d926e93b1
danielpi02 <saviodaniel26@gmail.com>
2024-08-13 00:05:35 +0100

    Atividade1
    Primeira atividade

---
+++ StaSi.py
@@ -0,0 +1,8 @@
+nome=input("Isira o seu nome: ") 
+idade=int(input("Insira a sua idade: "))
+
+
+if idade>=18:
+    print(f"{nome}, você é maior de idade")
+else:
+    print(f"{nome}, você é menor de idade")
